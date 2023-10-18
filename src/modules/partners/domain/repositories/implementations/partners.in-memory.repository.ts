451d167c1306a750a4fa8f20499f44d47c19abfd
After <contatonvastore@gmail.com>
2023-10-18 13:01:11 -0300

    refatoracao partner in memory

--- src/modules/partners/domain/repositories/implementations/partners.in-memory.repository.ts
+++ src/modules/partners/domain/repositories/implementations/partners.in-memory.repository.ts
@@ -2,71 +2,76 @@ import { AddPartnerDto } from 'src/modules/partners/http/dtos/add-partner.dto';
 import { UpdatePartnerDto } from 'src/modules/partners/http/dtos/update-partner.dto';
 import { Partner } from '../../entities/partner.entity';
 import { PartnerRepository } from '../partner.repository.interface';
-
 import { v4 as uuid } from 'uuid';
-import { HttpException } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class PartnersInMemoryRepository implements PartnerRepository {
-  private partner: Partner[] = [];
+  private partners: Partner[] = [];
 
-  async create(partner: Partner): Promise<void> {
-    const data = {
+  async create(partner: AddPartnerDto): Promise<void> {
+    const existingPartner = this.partners.find(
+      (p) => p.email === partner.email,
+    );
+    if (existingPartner) {
+      throw new HttpException(
+        'Partner with this email already exists',
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const newPartner: Partner = {
       id: uuid(),
       ...partner,
       updatedAt: new Date(),
       createdAt: new Date(),
     };
 
-    this.partner.push(data);
+    this.partners.push(newPartner);
   }
 
   async update(id: string, partner: UpdatePartnerDto): Promise<void> {
-    const index = this.partner.findIndex((item) => item.id === id);
+    const existingPartner = this.partners.find((p) => p.id === id);
 
-    this.partner[index] = {
-      ...this.partner[index],
-      ...partner,
-      updatedAt: new Date(),
-    };
+    if (!existingPartner) {
+      throw new HttpException('Partner not found', HttpStatus.NOT_FOUND);
+    }
+
+    Object.assign(existingPartner, partner);
+    existingPartner.updatedAt = new Date();
   }
 
-  async findByEmail(email: string): Promise<Partner> {
-    const partner = this.partner.find((item) => item.email === email);
-    return partner;
+  async findByEmail(email: string): Promise<Partner | undefined> {
+    return this.partners.find((p) => p.email === email);
   }
 
   async delete(id: string): Promise<void> {
-    const index = this.partner.findIndex((item) => item.id === id);
+    const index = this.partners.findIndex((p) => p.id === id);
 
     if (index === -1) {
-      throw new HttpException('Partner not found', 404);
+      throw new HttpException('Partner not found', HttpStatus.NOT_FOUND);
     }
 
-    this.partner.splice(index, 1);
+    this.partners.splice(index, 1);
   }
 
   async findAll(page: number, limit: number): Promise<Partner[]> {
-    const partners = this.partner.slice((page - 1) * limit, page * limit);
-    return partners;
+    const startIndex = (page - 1) * limit;
+    return this.partners.slice(startIndex, startIndex + limit);
   }
 
-  async findById(id: string): Promise<Partner> {
-    const partner = this.partner.find((item) => item.id === id);
-    return partner;
+  async findById(id: string): Promise<Partner | undefined> {
+    return this.partners.find((p) => p.id === id);
   }
 
-  async findByDocument(document: string): Promise<Partner> {
-    const partner = this.partner.find((item) => item.cnpj === document);
-    return partner;
+  async findByDocument(document: string): Promise<Partner | undefined> {
+    return this.partners.find((p) => p.cnpj === document);
   }
 
-  async findByCoverageArea(coverageArea: string): Promise<Partner> {
-    const partner = this.partner.find((item) => item.category === coverageArea);
-    return partner;
+  async findByCoverageArea(coverageArea: string): Promise<Partner | undefined> {
+    return this.partners.find((p) => p.category === coverageArea);
   }
 
-  async findByAddress(cep: string): Promise<Partner[]> {
-    const partners = this.partner.filter((item) => item.address.cep === cep);
-    return partners;
+  async findByAddress(cep: string): Promise<Partner[] | undefined> {
+    return this.partners.filter((p) => p.address.cep === cep);
   }
 }
