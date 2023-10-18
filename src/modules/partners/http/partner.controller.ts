49d2dcffca1c422bcaf7f267dd4842ed0bd277bd
After <contatonvastore@gmail.com>
2023-10-18 11:48:07 -0300

    implementacao do controller de partners

--- src/modules/partners/http/partner.controller.ts
+++ src/modules/partners/http/partner.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { PartnerService } from '../domain/partner.service';
+import { AddPartnerDto } from './dtos/add-partner.dto';
+import { Partner } from '../domain/entities/partner.entity';
 
 @Controller('partners')
-export class PartnerController {}
+export class PartnerController {
+  constructor(private readonly partnerService: PartnerService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async addPartner(@Body() partner: AddPartnerDto): Promise<void> {
+    await this.partnerService.addPartner(partner);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  async updatePartner(
+    @Param('id') id: string,
+    @Body() partner: AddPartnerDto,
+  ): Promise<void> {
+    await this.partnerService.updatePartner(id, partner);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async deletePartner(@Param('id') id: string): Promise<void> {
+    await this.partnerService.deletePartner(id);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<Partner[]> {
+    return await this.partnerService.findAllPartners();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findById(@Param('id') id: string): Promise<Partner> {
+    return await this.partnerService.findPartnerById(id);
+  }
+}
